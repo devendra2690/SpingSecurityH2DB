f26b3c16f0f8f1a92b52b1cf05e1fb2fec743c99
V-DPATIL <v-dpatil@revspringinc.com>
2021-07-12 16:35:14 +0530

    first commit

---
+++ src/main/java/io/javabrains/SpingSecurityH2DB/SpingSecurityH2DbApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.SpingSecurityH2DB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingSecurityH2DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingSecurityH2DbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/javabrains/SpingSecurityH2DB/SpingSecurityH2DbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.SpingSecurityH2DB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingSecurityH2DbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
